7871a0713e7f3fcdcb7cbdf1895b64e5047c652e
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-13 15:28:55 -0300

    Inicio do desafio

---
+++ desafio-6/src/main/java/br/com/mrocha/desafio6/DesafioBackend06Application.java
@@ -0,0 +1,13 @@
+package br.com.mrocha.desafio6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackend06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackend06Application.class, args);
+	}
+
+}
---
+++ desafio-6/src/test/java/br/com/mrocha/desafio6/DesafioBackend06ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mrocha.desafio6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackend06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

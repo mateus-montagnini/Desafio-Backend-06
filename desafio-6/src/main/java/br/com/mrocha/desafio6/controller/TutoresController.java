45c66ae1dd4fdd0ec3fbdd38f9e1151db729dc64
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-15 21:56:04 -0300

    Refatorando a classe de TutoresController

--- desafio-6/src/main/java/br/com/mrocha/desafio6/controller/TutoresController.java
+++ desafio-6/src/main/java/br/com/mrocha/desafio6/controller/TutoresController.java
@@ -1,16 +1,29 @@
 package br.com.mrocha.desafio6.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.mrocha.desafio6.model.DadosCadastroTutor;
+import br.com.mrocha.desafio6.model.Tutores;
+import br.com.mrocha.desafio6.repository.TutoresRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tutores")
 public class TutoresController {
 
-    @PostMapping
-    public void cadastrar() {
+    @Autowired
+    private TutoresRepository repository;
 
+    @PostMapping
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroTutor dados) {
+
+        if (!dados.senha().equals(dados.confirmacaoSenha())) {
+            return ResponseEntity.badRequest().body("Senha precisa ser igual a confirmacao de senha");
+        }
+
+        repository.save(new Tutores(dados));
+
+        return ResponseEntity.ok().body("Tutor cadastrado com sucesso");
     }
 }

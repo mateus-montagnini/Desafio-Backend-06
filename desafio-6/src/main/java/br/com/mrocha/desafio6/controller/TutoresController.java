041d33075aa33e93c237fe161f39bb1cbd7c64ca
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-16 20:28:58 -0300

    Criando o metodo para listar todos os tutores

--- desafio-6/src/main/java/br/com/mrocha/desafio6/controller/TutoresController.java
+++ desafio-6/src/main/java/br/com/mrocha/desafio6/controller/TutoresController.java
@@ -1,12 +1,18 @@
 package br.com.mrocha.desafio6.controller;
 
 import br.com.mrocha.desafio6.model.DadosCadastroTutor;
+import br.com.mrocha.desafio6.model.DadosDetalheTutor;
+import br.com.mrocha.desafio6.model.DadosTutor;
 import br.com.mrocha.desafio6.model.Tutores;
 import br.com.mrocha.desafio6.repository.TutoresRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/tutores")
@@ -22,8 +28,18 @@ public class TutoresController {
             return ResponseEntity.badRequest().body("Senha precisa ser igual a confirmacao de senha");
         }
 
-        repository.save(new Tutores(dados));
+        Tutores tutor = new Tutores(dados);
+        repository.save(tutor);
 
-        return ResponseEntity.ok().body("Tutor cadastrado com sucesso");
+        return ResponseEntity.ok(new DadosTutor(tutor));
+    }
+
+    @GetMapping
+    public ResponseEntity listarTodos() {
+        List<Tutores> tutores = repository.findAll();
+        if (tutores.isEmpty()) {
+            return ResponseEntity.ok("nao encontrado");
+        }
+        return ResponseEntity.ok(tutores.stream().map(DadosDetalheTutor::new).collect(Collectors.toList()));
     }
 }

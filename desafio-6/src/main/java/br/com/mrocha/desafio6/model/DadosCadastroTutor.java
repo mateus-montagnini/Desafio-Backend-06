11a7d2f322af8ea11d44ab0750c13d8399749772
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-16 20:28:35 -0300

    Refatorando o record de DadosCadastroTutor

--- desafio-6/src/main/java/br/com/mrocha/desafio6/model/DadosCadastroTutor.java
+++ desafio-6/src/main/java/br/com/mrocha/desafio6/model/DadosCadastroTutor.java
@@ -2,10 +2,14 @@ package br.com.mrocha.desafio6.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public record DadosCadastroTutor(
         @NotBlank String nome,
         @NotBlank @Email String email,
         @NotBlank String senha,
         @NotBlank String confirmacaoSenha) {
+
 }

0962c06f88561c23a17330f553ddf0c6c93b6050
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-15 21:55:57 -0300

    Refatorando a classe de Tutores

--- desafio-6/src/main/java/br/com/mrocha/desafio6/model/Tutores.java
+++ desafio-6/src/main/java/br/com/mrocha/desafio6/model/Tutores.java
@@ -1,8 +1,15 @@
 package br.com.mrocha.desafio6.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 @Entity
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString(of = {"id", "nome", "email"})
 @Table(name = "tb_tutores")
 public class Tutores {
 
@@ -18,4 +25,10 @@ public class Tutores {
 
     @Column(name = "senha")
     private String senha;
+
+    public Tutores(DadosCadastroTutor dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.senha = dados.senha();
+    }
 }

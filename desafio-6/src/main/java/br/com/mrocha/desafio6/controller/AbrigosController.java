a6573c0494ec4afc43bd5c2ff697f09763d99bc2
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-19 19:40:01 -0300

    Criando o controller do Abrigo

---
+++ desafio-6/src/main/java/br/com/mrocha/desafio6/controller/AbrigosController.java
@@ -0,0 +1,77 @@
+package br.com.mrocha.desafio6.controller;
+
+import br.com.mrocha.desafio6.abrigoModel.Abrigo;
+import br.com.mrocha.desafio6.abrigoModel.DadosAbrigo;
+import br.com.mrocha.desafio6.abrigoModel.DadosAtualizacaoAbrigo;
+import br.com.mrocha.desafio6.abrigoModel.DadosCadastroAbrigo;
+import br.com.mrocha.desafio6.repository.AbrigosRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/abrigos")
+public class AbrigosController {
+
+    @Autowired
+    private AbrigosRepository repository;
+
+    @PostMapping
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroAbrigo dados) {
+
+        Abrigo abrigo = new Abrigo(dados);
+        repository.save(abrigo);
+
+        return ResponseEntity.ok(new DadosAbrigo(abrigo));
+    }
+
+    @GetMapping
+    public ResponseEntity listarTodos() {
+        List<Abrigo> abrigos = repository.findAll();
+        if (abrigos.isEmpty()) {
+            return ResponseEntity.ok("nao encontrado");
+        }
+        return ResponseEntity.ok(abrigos.stream().map(DadosAbrigo::new).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity listarPorId(@PathVariable Long id) {
+        try {
+            var abrigo = repository.getReferenceById(id);
+            return ResponseEntity.ok(new DadosAbrigo(abrigo));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.ok("Abrigo nao encontrado");
+
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletarTutor(@PathVariable Long id) {
+        Optional<Abrigo> abrigo = repository.findById(id);
+        if (abrigo.isPresent()) {
+            repository.deleteById(id);
+            return ResponseEntity.ok("Abrigo apagado com sucesso");
+        }
+        return ResponseEntity.ok("Abrigo nao encontrado");
+    }
+
+    @RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH})
+    @Transactional
+    public ResponseEntity atualizarTutor(@RequestBody @Valid DadosAtualizacaoAbrigo dados) {
+        try {
+            var abrigo = repository.getReferenceById(dados.id());
+            abrigo.atualizarDados(dados);
+
+            return ResponseEntity.ok(new DadosAbrigo(abrigo));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.ok("Abrigo nao encontrado");
+        }
+    }
+}

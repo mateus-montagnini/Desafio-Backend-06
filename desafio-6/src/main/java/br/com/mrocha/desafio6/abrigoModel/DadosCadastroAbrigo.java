e5e5b8415ab312a61c8f884d658bfb998105edd5
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-19 21:59:25 -0300

    Criando o DadosCadastroAbrigo

--- desafio-6/src/main/java/br/com/mrocha/desafio6/abrigoModel/DadosCadastroAbrigo.java
+++ desafio-6/src/main/java/br/com/mrocha/desafio6/abrigoModel/DadosCadastroAbrigo.java
@@ -1,8 +1,11 @@
 package br.com.mrocha.desafio6.abrigoModel;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
 public record DadosCadastroAbrigo(
-        String nome,
-        String email,
-        String telefone
+        @NotNull String nome,
+        @NotNull @Email String email,
+        @NotNull String telefone
 ) {
 }

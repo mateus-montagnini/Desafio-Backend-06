8445c21338a0f1ebc18c9e409b02c6e2aa8609ed
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-20 20:13:10 -0300

    Criando o controller de Adocao

---
+++ desafio-6/src/main/java/br/com/mrocha/desafio6/controller/AdocaoController.java
@@ -0,0 +1,38 @@
+package br.com.mrocha.desafio6.controller;
+
+import br.com.mrocha.desafio6.domain.Adocao.Adocao;
+import br.com.mrocha.desafio6.domain.Adocao.DadosAdocao;
+import br.com.mrocha.desafio6.domain.Adocao.DadosCadastroAdocao;
+import br.com.mrocha.desafio6.domain.Pets.Pets;
+import br.com.mrocha.desafio6.repository.PetsRepository;
+import br.com.mrocha.desafio6.repository.TutoresRepository;
+import br.com.mrocha.desafio6.service.AdocaoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pets")
+public class AdocaoController {
+
+    @Autowired
+    private AdocaoService service;
+
+    @PostMapping
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroAdocao dados) {
+        Adocao adocao = service.cadastraAdocao(dados);
+
+        return ResponseEntity.ok(new DadosAdocao(adocao));
+    }
+
+        @DeleteMapping("/{id}")
+    public ResponseEntity deletarTutor(@PathVariable Long id) {
+        service.deletarAdocao(id);
+
+        return ResponseEntity.ok("Adocao removida");
+    }
+
+}

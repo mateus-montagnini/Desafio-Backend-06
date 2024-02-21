1e7e65f48170d2f36de5a7c4ee69dadbf6cab041
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-21 19:46:12 -0300

    Refatorando a classe de TratadorExceptionHandler

--- desafio-6/src/main/java/br/com/mrocha/desafio6/infra/exception/TratadorExceptionHandler.java
+++ desafio-6/src/main/java/br/com/mrocha/desafio6/infra/exception/TratadorExceptionHandler.java
@@ -4,13 +4,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class TratadorExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity tratarErroDaValidacao(IllegalArgumentException e) {
 
-        return ResponseEntity.badRequest().body(e.getMessage());
+        return ResponseEntity.badRequest().body(new DadosErro400(LocalDateTime.now(), e.getMessage()));
+
+    }
+
+    private record DadosErro400(LocalDateTime timestamp, String message) {
+
 
     }
 }

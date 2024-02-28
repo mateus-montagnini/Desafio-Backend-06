ebba3fe9e4e103fea7f00298f13a082274de8173
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-28 15:03:00 -0300

    Refatorando a classe de DadosCadastroAbrigo

--- desafio-6/src/main/java/br/com/mrocha/desafio6/domain/Abrigo/DadosCadastroAbrigo.java
+++ desafio-6/src/main/java/br/com/mrocha/desafio6/domain/Abrigo/DadosCadastroAbrigo.java
@@ -1,10 +1,13 @@
 package br.com.mrocha.desafio6.domain.Abrigo;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record DadosCadastroAbrigo(
         @NotNull String nome,
+        @NotBlank String login,
+        @NotBlank String senha,
         @NotNull @Email String email,
         @NotNull String telefone
 ) {

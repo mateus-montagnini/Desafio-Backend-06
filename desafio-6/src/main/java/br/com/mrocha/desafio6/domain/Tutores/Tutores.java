3d430ef15e04763b0ecd4eb13e27746707bb2126
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-28 15:02:43 -0300

    Refatorando a classe de Tutores

--- desafio-6/src/main/java/br/com/mrocha/desafio6/domain/Tutores/Tutores.java
+++ desafio-6/src/main/java/br/com/mrocha/desafio6/domain/Tutores/Tutores.java
@@ -14,28 +14,20 @@ import lombok.*;
 @Table(name = "tb_tutores")
 public class Tutores extends Usuario {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     @Column(name = "nome")
     private String nome;
 
-    @Column(name = "email")
-    private String email;
-
-    @Column(name = "senha")
-    private String senha;
 
     public Tutores(DadosCadastroTutor dados) {
         this.nome = dados.nome();
         this.email = dados.email();
+        this.login = dados.login();
         this.senha = dados.senha();
     }
 
     public void atualizarDados(DadosAtualizacaoTutor dados) {
         this.nome = dados.nome() != null ? dados.nome() : this.nome;
         this.email = dados.email() != null ? dados.email() : this.email;
-        this.senha = dados.senha() != null ? dados.senha() : this.senha;
     }
 }

ac745ca7415a0a77aad186b1cdb5719f06e596fc
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-03-01 14:45:26 -0300

    Finalizando o SecurityConf

--- desafio-6/src/main/java/br/com/mrocha/desafio6/infra/security/SecurityConf.java
+++ desafio-6/src/main/java/br/com/mrocha/desafio6/infra/security/SecurityConf.java
@@ -1,5 +1,6 @@
 package br.com.mrocha.desafio6.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConf {
+
+    @Autowired
+    private JWTFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -23,6 +28,7 @@ public class SecurityConf {
                         .anyRequest().permitAll() //authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
